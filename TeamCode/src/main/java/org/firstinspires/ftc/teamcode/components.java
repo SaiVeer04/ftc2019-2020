a42dbcb7ffb61da358760e187a56c683d8512434
saiveer04 <saiveer2001@gmail.com>
2019-10-17 19:30:00 -0400

    new auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components.java
@@ -81,7 +81,7 @@ public class components{
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       /* fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -96,7 +96,7 @@ public class components{
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);*/
 
 
 
