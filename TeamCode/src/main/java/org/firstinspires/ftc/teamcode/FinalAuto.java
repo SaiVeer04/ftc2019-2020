bd2eabafce9c34e58a9e3e5e5912ef488012d9a5
Karthik-Kundurthy <karthikkundurthy@gmail.com>
2019-10-18 21:56:11 -0400

    Hello world

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalAuto.java
@@ -120,8 +120,8 @@ public class FinalAuto extends LinearOpMode {
         telemetry.addData(">", "Press Play to start op mode");
         telemetry.update();
         waitForStart();
-        encoderDrive(.3,-28.5,-28.5,-28.5,-28.5,3);
-        robot.foundation.setPosition(0);
+        encoderDrive(.3,-28.3,-28.3,-28.3,-28.3,3);
+        robot.foundation.setPosition(.5);
 
 
         if (opModeIsActive()) {
