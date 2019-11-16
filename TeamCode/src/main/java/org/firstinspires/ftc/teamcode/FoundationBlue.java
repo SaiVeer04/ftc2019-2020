0d672e77ffc5551d0aac02ebb8b9519a63c96fc8
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2019-11-16 14:45:15 -0500

    adding movement logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationBlue.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name = "FoundationRed")
+@Autonomous(name = "FoundationBlue")
 public class FoundationBlue extends LinearOpMode {
     componentsHardCode robot = new componentsHardCode();
     public void runOpMode() throws InterruptedException{
@@ -20,9 +20,7 @@ public class FoundationBlue extends LinearOpMode {
             }
             sleep(500);
             //strafe to the middle
-            strafeRight(1, 700, 500);
-            //move to  align
-            moveBackWard(.3,500,500);
+            strafeRight(1, 350, 500);
             //latch on
             robot.foundation.setPower(-0.5);
             Thread.sleep(2000);
@@ -35,13 +33,13 @@ public class FoundationBlue extends LinearOpMode {
             robot.foundation.setPower(0.5);
             Thread.sleep(2000);
             sleep(500);
-            strafeLeft(1,700,500);
+            strafeLeft(1,1700,500);
             moveForward(.4,700,500);
-            moveBackWard(.4,1350,500);
+            moveBackWard(.4,1150,500);
             strafeRight(1,1050,500);
             while(true){
                 if(robot.sensorColor.red() < 40){
-                    strafeLeft(1,75,0);
+                    strafeRight(1,75,0);
                 }else if(robot.sensorColor.red() < 40){
                     break;
                 }
