b249af5aeb81b1c8e2c983522a88348c66805023
saiveer04 <saiveer2001@gmail.com>
2020-01-25 09:06:38 -0500

    yala habibi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoEncoderTest.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Color;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -10,7 +15,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
@@ -18,6 +27,12 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 @Autonomous(name = "EncoderHard")
 public class AutoEncoderTest extends LinearOpMode{
+
+    float hsvValues[] = {0F, 0F, 0F};
+
+    // sometimes it helps to multiply the raw RGB values with a scale factor
+    // to amplify/attentuate the measured values.
+    final double SCALE_FACTOR = 255;
     // we use customary because we cool like that
     //all holding values
     static final double wheel_diameter = 4.0;
@@ -31,12 +46,48 @@ public class AutoEncoderTest extends LinearOpMode{
     public DcMotor fr;
     public DcMotor br;
 
-    public void runOpMode() {
+    public ColorSensor colorSensor;
+
+    public DcMotor intakeLeft;
+    //intakeright
+    public DcMotor intakeRight;
+
+    // The IMU sensor object
+    BNO055IMU imu;
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
+
+
+
+    public void runOpMode() throws InterruptedException{
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
 
         bl = hardwareMap.dcMotor.get("BL");
         fl = hardwareMap.dcMotor.get("FL");
         fr = hardwareMap.dcMotor.get("FR");
         br = hardwareMap.dcMotor.get("BR");
+
+        intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
+        intakeRight = hardwareMap.dcMotor.get("intakeRight");
+
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+
 
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -48,7 +99,85 @@ public class AutoEncoderTest extends LinearOpMode{
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
-        backwards(31,.5);
+
+        backwards(22,.5);
+        sleep(100);
+
+
+        for (int i = 0; i <= 12; i++) {
+            sleep(500);
+            Color.RGBToHSV((int) (colorSensor.red() * SCALE_FACTOR),
+                    (int) (colorSensor.green() * SCALE_FACTOR),
+                    (int) (colorSensor.blue() * SCALE_FACTOR),
+                    hsvValues);
+            sleep(500);
+
+            if (hsvValues[0] > 80) { //check the hue value
+                straferight(5,.5);
+
+                sleep(500);
+
+                fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                fl.setPower(-.5);
+                fr.setPower(-.5);
+                bl.setPower(-.5);
+                br.setPower(-.5);
+
+                intakeLeft.setPower(-0.7); //make the intake wheels move
+                intakeRight.setPower(0.7);
+
+                Thread.sleep(400);
+                fl.setPower(0);
+                bl.setPower(0);
+                fr.setPower(0);
+                br.setPower(0);
+
+                intakeLeft.setPower(0); //stop the intake wheels
+                intakeRight.setPower(0);
+
+                sleep(500);
+
+                intakeLeft.setPower(-0.7); //make the intake wheels move
+                intakeRight.setPower(0.7);
+
+                Thread.sleep(400);
+
+                sleep(500);
+
+
+
+                fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+
+                break;
+
+            }else{
+                straferight(3,.5);
+                while(true) {
+                    angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                    telemetry.addData("Heading:" , angles.firstAngle);
+                    if(angles.firstAngle > 8){
+                        turn(5,"l",.3);
+                    }else if(angles.firstAngle < 3){
+                        turn(5,"r",.3);
+                    }else{
+                        break;
+                    }
+                    telemetry.update();
+                }
+            }
+        }
+
 
     }
     public void reset_motor(){
