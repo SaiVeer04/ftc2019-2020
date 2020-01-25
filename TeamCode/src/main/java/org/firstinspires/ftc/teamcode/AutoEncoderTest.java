6e0a3b693f397bc854180c29497f095a2e2ea628
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2020-01-25 14:56:25 -0500

    adding movement logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoEncoderTest.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -13,12 +14,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
@@ -52,6 +56,13 @@ public class AutoEncoderTest extends LinearOpMode{
     //intakeright
     public DcMotor intakeRight;
 
+    public DistanceSensor sensorRange;
+    public DistanceSensor sensorRange1;
+
+    public Servo leftServo;
+
+    public Servo rightServo;
+
     // The IMU sensor object
     BNO055IMU imu;
 
@@ -61,14 +72,14 @@ public class AutoEncoderTest extends LinearOpMode{
 
 
 
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
         // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
@@ -86,6 +97,17 @@ public class AutoEncoderTest extends LinearOpMode{
         intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
         intakeRight = hardwareMap.dcMotor.get("intakeRight");
 
+        leftServo = hardwareMap.get(Servo.class, "leftServo");
+
+        rightServo = hardwareMap.get(Servo.class, "rightServo");
+
+
+        sensorRange = hardwareMap.get(DistanceSensor.class, "sensor_range");
+        Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorRange;
+
+        sensorRange1 = hardwareMap.get(DistanceSensor.class, "sensor_range1");
+        Rev2mDistanceSensor sensorTimeOfFlight1 = (Rev2mDistanceSensor)sensorRange1;
+
         colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
 
 
@@ -98,87 +120,214 @@ public class AutoEncoderTest extends LinearOpMode{
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        double movement = 0;
+
         waitForStart();
 
-        backwards(22,.5);
-        sleep(100);
+        if (opModeIsActive()) {
+
+            backwards(22, .5);
+            sleep(100);
 
 
-        for (int i = 0; i <= 12; i++) {
-            sleep(500);
-            Color.RGBToHSV((int) (colorSensor.red() * SCALE_FACTOR),
-                    (int) (colorSensor.green() * SCALE_FACTOR),
-                    (int) (colorSensor.blue() * SCALE_FACTOR),
-                    hsvValues);
-            sleep(500);
-
-            if (hsvValues[0] > 80) { //check the hue value
-                straferight(5,.5);
-
+            for (int i = 0; i <= 12; i++) {
+                sleep(500);
+                Color.RGBToHSV((int) (colorSensor.red() * SCALE_FACTOR),
+                        (int) (colorSensor.green() * SCALE_FACTOR),
+                        (int) (colorSensor.blue() * SCALE_FACTOR),
+                        hsvValues);
                 sleep(500);
 
-                fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                if (hsvValues[0] > 80) { //check the hue value
+                    straferight(5, .5);
 
-                fl.setPower(-.5);
-                fr.setPower(-.5);
-                bl.setPower(-.5);
-                br.setPower(-.5);
+                    sleep(500);
 
-                intakeLeft.setPower(-0.7); //make the intake wheels move
-                intakeRight.setPower(0.7);
+                    fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-                Thread.sleep(400);
-                fl.setPower(0);
-                bl.setPower(0);
-                fr.setPower(0);
-                br.setPower(0);
+                    fl.setPower(-.5);
+                    fr.setPower(-.5);
+                    bl.setPower(-.5);
+                    br.setPower(-.5);
 
-                intakeLeft.setPower(0); //stop the intake wheels
-                intakeRight.setPower(0);
+                    intakeLeft.setPower(-0.7); //make the intake wheels move
+                    intakeRight.setPower(0.7);
 
-                sleep(500);
+                    Thread.sleep(400);
+                    fl.setPower(0);
+                    bl.setPower(0);
+                    fr.setPower(0);
+                    br.setPower(0);
 
-                intakeLeft.setPower(-0.7); //make the intake wheels move
-                intakeRight.setPower(0.7);
+                    intakeLeft.setPower(0); //stop the intake wheels
+                    intakeRight.setPower(0);
 
-                Thread.sleep(400);
+                    sleep(100);
 
-                sleep(500);
+                    intakeLeft.setPower(-0.7); //make the intake wheels move
+                    intakeRight.setPower(0.7);
+
+                    fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                    fl.setPower(-.5);
+                    fr.setPower(-.5);
+                    bl.setPower(-.5);
+                    br.setPower(-.5);
+
+                    Thread.sleep(200);
+
+                    intakeLeft.setPower(0); //stop intake wheels
+                    intakeRight.setPower(0);
+
+                    fl.setPower(0);
+                    bl.setPower(0);
+                    fr.setPower(0);
+                    br.setPower(0);
+
+                    sleep(500);
 
 
+                    fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-                fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    forward(29, 0.5); //moves the robot back
+                    sleep(200);
+
+                    //to turn right
+                    int final_back = (int) Math.round(18 * ticks_per_inch);
+
+                    reset_motor();
+                    fl.setTargetPosition(-final_back);
+                    bl.setTargetPosition(-final_back);
+                    fr.setTargetPosition(+final_back);
+                    br.setTargetPosition(+final_back);
+                    powerBusy(0.5);
 
 
+                    sleep(200);
+
+                    backwards(70 + movement, 0.5); //move to the foundation side (68 was original value)
+
+                    //to turn left
+                    int final_back1 = (int) Math.round(17 * ticks_per_inch);
+
+                    reset_motor();
+                    fl.setTargetPosition(+final_back1);
+                    bl.setTargetPosition(+final_back1);
+                    fr.setTargetPosition(-final_back1);
+                    br.setTargetPosition(-final_back1);
+                    powerBusy(0.5);
+
+                    sleep(500);
 
 
+                    backwards(10, 0.3); //move forward to the foundation
 
-                break;
-
-            }else{
-                straferight(3,.5);
-                while(true) {
-                    angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-                    telemetry.addData("Heading:" , angles.firstAngle);
-                    if(angles.firstAngle > 8){
-                        turn(5,"l",.3);
-                    }else if(angles.firstAngle < 3){
-                        turn(5,"r",.3);
-                    }else{
-                        break;
+                    while (true) {
+                        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                        telemetry.addData("Heading:", angles.firstAngle);
+                        if (angles.firstAngle > 5) {
+                            turn(5, "l", .3);
+                        } else if (angles.firstAngle < 3) {
+                            turn(5, "r", .3);
+                        } else {
+                            break;
+                        }
+                        telemetry.update();
                     }
-                    telemetry.update();
+
+
+                    leftServo.setPosition(-0.5); //clamp on
+                    rightServo.setPosition(0.4);
+                    sleep(2500);
+
+
+                    forward(28, 0.5); //move backward
+                    sleep(200);
+
+                    //turn right
+                    int final_back2 = (int) Math.round(52 * ticks_per_inch);
+
+                    reset_motor();
+                    fl.setTargetPosition(-final_back2);
+                    bl.setTargetPosition(-final_back2);
+                    fr.setTargetPosition(+final_back2);
+                    br.setTargetPosition(+final_back2);
+                    powerBusy(0.5);
+
+
+
+                    leftServo.setPosition(1); //let go
+                    rightServo.setPosition(0);
+                    sleep(300);
+
+                    backwards(4, 0.5); //bang into the wall
+                    sleep(500);
+
+                    forward(2, 0.4); //move a little backwards to relieve friction before strafing
+                    sleep(200);
+
+                    straferight(15, 0.5); //move to the side of the foundation
+                    sleep(500);
+
+                    backwards(7, 0.5);
+                    fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                    fl.setPower(.5);
+                    fr.setPower(.5);
+                    bl.setPower(.5);
+                    br.setPower(.5);
+
+                    intakeLeft.setPower(0.7); //outtake
+                    intakeRight.setPower(-0.7);
+                    sleep(600);
+                    intakeLeft.setPower(0);
+                    intakeRight.setPower(0);
+
+                    fl.setPower(0);
+                    fr.setPower(0);
+                    bl.setPower(0);
+                    br.setPower(0);
+
+                    forward(17, 0.5); //park
+
+
+
+
+
+                    break;
+
+                } else {
+                    movement += 5;
+                    straferight(5, .5);
+                    while (true) {
+                        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                        telemetry.addData("Heading:", angles.firstAngle);
+                        if (angles.firstAngle > 8) {
+                            turn(5, "l", .3);
+                        } else if (angles.firstAngle < 3) {
+                            turn(5, "r", .3);
+                        } else {
+                            break;
+                        }
+                        telemetry.update();
+                    }
                 }
             }
+
+
         }
-
-
     }
     public void reset_motor(){
         //resets encoders
@@ -274,9 +423,7 @@ public class AutoEncoderTest extends LinearOpMode{
 
         }
 
-
-
-
     }
+
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dualRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dualRed.java
@@ -4,12 +4,20 @@ import android.graphics.Color;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous(name = "Bin Laden")
+
 public class dualRed extends LinearOpMode {
     componentsHardCode robot = new componentsHardCode();
+
+    public DcMotor bl;
+    public DcMotor fl;
+    public DcMotor fr;
+    public DcMotor br;
 
     float hsvValues[] = {0F, 0F, 0F};
 
@@ -17,247 +25,135 @@ public class dualRed extends LinearOpMode {
     // to amplify/attentuate the measured values.
     final double SCALE_FACTOR = 255;
 
+    static final double wheel_diameter = 4.0;
+    static final double gear_ratio = 1.0;
+    //for encoders
+    static final double ticks = 537.6;
+    static final double ticks_per_inch = (ticks * gear_ratio) / (wheel_diameter * Math.PI);
+
 
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
 
+        bl = hardwareMap.dcMotor.get("BL");
+        fl = hardwareMap.dcMotor.get("FL");
+        fr = hardwareMap.dcMotor.get("FR");
+        br = hardwareMap.dcMotor.get("BR");
+
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //required to tell using encoder
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart(); //when the driver clicks play
+        backwards(31, 0.5);
 
-        if (opModeIsActive()) {
-            while (robot.sensorRange.getDistance(DistanceUnit.CM) > 2) {
-                robot.fl.setPower(0.7);
-                robot.fr.setPower(0.7);
-                robot.bl.setPower(-0.7);
-                robot.br.setPower(-0.7);
-                if (robot.sensorRange.getDistance(DistanceUnit.CM) <= 2) {
-                    stopRobot();
-                    break;
-                }
-            }
-
-
-            for (int i = 0; i <= 12; i++) {
-                sleep(500);
-                Color.RGBToHSV((int) (robot.colorSensor.red() * SCALE_FACTOR),
-                        (int) (robot.colorSensor.green() * SCALE_FACTOR),
-                        (int) (robot.colorSensor.blue() * SCALE_FACTOR),
-                        hsvValues);
-                sleep(500);
-
-                if (hsvValues[0] > 80) { //check the hue value
-
-                    strafeRight(0.5, 200, 20); //to align properly
-                    moveForward(0.5, 50, 200);
-                    robot.intakeLeft.setPower(-0.7); //make the intake wheels move
-                    robot.intakeRight.setPower(0.7);
-                    moveBackward(0.2, 1200, 400); //move the robot
-                    robot.intakeLeft.setPower(0); //stop the intake wheels
-                    robot.intakeRight.setPower(0);
-                    break;
-
-                } else {
-                    strafeRight(0.5, 250, 20);
-                    robot.fl.setPower(-0.5); //to turn
-                    robot.fr.setPower(0.5);
-                    robot.bl.setPower(0.5);
-                    robot.br.setPower(-0.5);
-                    sleep(10);
-                    robot.fl.setPower(0);
-                    robot.bl.setPower(0);
-                    robot.fr.setPower(0);
-                    robot.br.setPower(0);
-                }
-            }
-
-            moveForward(0.5, 1000, 200); //move backward
-
-            robot.fl.setPower(-0.5); //to turn
-            robot.fr.setPower(0.5);
-            robot.bl.setPower(0.5);
-            robot.br.setPower(-0.5);
-            sleep(800);
-            robot.fl.setPower(0);
-            robot.bl.setPower(0);
-            robot.fr.setPower(0);
-            robot.br.setPower(0);
-
-            moveForward(0.5, 2500, 200); //move past the line
-
-            robot.intakeLeft.setPower(0.7); //make the intake wheels move
-            robot.intakeRight.setPower(-0.7);
-            sleep(300);
-            robot.intakeLeft.setPower(0); //stop the intake wheels
-            robot.intakeRight.setPower(0);
-
-
-            strafeRight(0.5, 1200, 200); //move away from the block
-
-            while (robot.bottomSensor.blue() < 200) {
-                moveBackward(0.3, 350, 10);
-                if (robot.bottomSensor.blue() > 75) {
-                    stopRobot();
-                    break;
-                }
-
-
-            }
-
-            //SECOND PART
-            sleep(1000);
-
-            while (robot.sensorRange1.getDistance(DistanceUnit.CM) > 2) {
-                moveBackward(0.2, 200, 0);
-                if (robot.sensorRange1.getDistance(DistanceUnit.CM) <= 2) {
-                    stopRobot();
-                    break;
-                }
-            }
-
-            strafeLeft(0.3, 1000, 200); //strafe to the middle
-
-            robot.fl.setPower(0.3);
-            robot.bl.setPower(-0.3);
-            robot.fr.setPower(-0.3);
-            robot.br.setPower(0.3);
-            sleep(50);
-            robot.fl.setPower(0);
-            robot.bl.setPower(0);
-            robot.fr.setPower(0);
-            robot.br.setPower(0);
-
-            /*while (robot.sensorRange1.getDistance(DistanceUnit.CM) > 1) { //move up
-                moveBackward(0.2, 100, 0);
-                if (robot.sensorRange1.getDistance(DistanceUnit.CM) <= 1) {
-                    stopRobot();
-                    break;
-                }
-            }*/
-
-            //distanceCheck();
-
-            moveBackward(.4,300,200); //move closer to the robot because strafing moves ht robot backward
-
-            /*while (robot.sensorRange1.getDistance(DistanceUnit.CM) > 2) { //move up
-                moveBackward(0.2, 100, 0);
-                if (robot.sensorRange1.getDistance(DistanceUnit.CM) <= 2) {
-                    stopRobot();
-                    break;
-                }
-            }*/
-
-
-            robot.leftServo.setPosition(-0.5); //clamp on
-            robot.rightServo.setPosition(0.4);
-            sleep(2500);
-
-
-            moveForward(0.4, 3000, 200); //move backward
-
-            robot.leftServo.setPosition(1); //let go
-            robot.rightServo.setPosition(0);
-            sleep(300);
-
-            moveBackward(0.5, 100, 500); //move up a little to avoid friction
-
-
-            strafeRight(0.5, 2500, 200); //move parallel
-
-            moveBackward(0.5, 500, 500); //move to the side
-
-            strafeLeft(0.5, 2500, 500); //push the foundation in
-
-            moveForward(0.3, 400, 200); //move backward a bit
-
-
-        }
 
     }
 
-    public void moveForward(double power, int movement, int sleep) throws InterruptedException {
-        robot.fl.setPower(-power);
-        robot.fr.setPower(-power);
-        robot.bl.setPower(power);
-        robot.br.setPower(power);
-        Thread.sleep(movement);
-        robot.fl.setPower(0);
-        robot.bl.setPower(0);
-        robot.fr.setPower(0);
-        robot.br.setPower(0);
-        sleep(sleep);
+    public void reset_motor() {
+        //resets encoders
+        robot.fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    public void moveBackward(double power, int movement, int sleep) throws InterruptedException {
+    //this one lets each movement run to the full extent with out overriding
+    public void powerBusy(double power) {
+        //lets program run fully
         robot.fl.setPower(power);
         robot.fr.setPower(power);
-        robot.bl.setPower(-power);
-        robot.br.setPower(-power);
-        Thread.sleep(movement);
-        robot.fl.setPower(0);
-        robot.bl.setPower(0);
-        robot.fr.setPower(0);
-        robot.br.setPower(0);
-        sleep(sleep);
-    }
-
-    public void strafeLeft(double power, int movement, int sleep) throws InterruptedException {
-        robot.fl.setPower(+power);
-        robot.fr.setPower(-power);
-        robot.bl.setPower(+power);
-        robot.br.setPower(-power);
-        Thread.sleep(movement);
-        robot.fl.setPower(0);
-        robot.bl.setPower(0);
-        robot.fr.setPower(0);
-        robot.br.setPower(0);
-        sleep(sleep);
-    }
-
-    public void strafeRight(double power, int movement, int sleep) throws InterruptedException {
-        robot.fl.setPower(-power);
-        robot.fr.setPower(+power);
-        robot.bl.setPower(-power);
-        robot.br.setPower(+power);
-        Thread.sleep(movement);
-        robot.fl.setPower(0);
-        robot.bl.setPower(0);
-        robot.fr.setPower(0);
-        robot.br.setPower(0);
-        sleep(sleep);
-    }
-
-    public void stopRobot() {
+        robot.bl.setPower(power);
+        robot.br.setPower(power);
+        while ((fl.isBusy() && fr.isBusy()) && (bl.isBusy() && br.isBusy())) {
+        }
         robot.fl.setPower(0);
         robot.fr.setPower(0);
         robot.bl.setPower(0);
         robot.br.setPower(0);
     }
 
-    public void distanceCheck() throws InterruptedException {
-        while (robot.sensorRange.getDistance(DistanceUnit.CM) != robot.sensorRange1.getDistance(DistanceUnit.CM)) {
-            if (robot.sensorRange.getDistance(DistanceUnit.CM) > robot.sensorRange1.getDistance(DistanceUnit.CM)) {
-                robot.fl.setPower(0.1); //turn left
-                robot.bl.setPower(-0.1);
-                robot.fr.setPower(-0.1);
-                robot.br.setPower(0.1);
-                Thread.sleep(100);
-                robot.fl.setPower(0);
-                robot.bl.setPower(0);
-                robot.fr.setPower(0);
-                robot.br.setPower(0);
-            } else if (robot.sensorRange.getDistance(DistanceUnit.CM) < robot.sensorRange1.getDistance(DistanceUnit.CM)) {
-                robot.fl.setPower(-0.1); //turn right
-                robot.bl.setPower(0.1);
-                robot.fr.setPower(0.1);
-                robot.br.setPower(-0.1);
-                Thread.sleep(100);
-                robot.fl.setPower(0);
-                robot.bl.setPower(0);
-                robot.fr.setPower(0);
-                robot.br.setPower(0);
-            } else {
-                break;
-            }
+    public void forward(double front, double power) {
+        int final_front = (int) Math.round(front * ticks_per_inch);
+
+        reset_motor();
+        fl.setTargetPosition(final_front);
+        bl.setTargetPosition(final_front);
+        fr.setTargetPosition(final_front);
+        br.setTargetPosition(final_front);
+        powerBusy(power);
+
+    }
+
+    public void backwards(double back, double power) {
+
+        int final_back = (int) Math.round(back * ticks_per_inch);
+
+        reset_motor();
+        fl.setTargetPosition(-final_back);
+        bl.setTargetPosition(-final_back);
+        fr.setTargetPosition(-final_back);
+        br.setTargetPosition(-final_back);
+        powerBusy(power);
+
+    }
+
+    public void strafeleft(double back, double power) {
+
+        int final_back = (int) Math.round(back * ticks_per_inch);
+
+        reset_motor();
+        fl.setTargetPosition(-final_back);
+        bl.setTargetPosition(final_back);
+        fr.setTargetPosition(final_back);
+        br.setTargetPosition(-final_back);
+        powerBusy(power);
+
+    }
+
+    public void straferight(double back, double power) {
+
+        int final_back = (int) Math.round(back * ticks_per_inch);
+
+        reset_motor();
+        fl.setTargetPosition(final_back);
+        bl.setTargetPosition(-final_back);
+        fr.setTargetPosition(-final_back);
+        br.setTargetPosition(final_back);
+        powerBusy(power);
+
+    }
+
+    public void turn(double degrees, String l_or_r, double power) {
+        if (l_or_r.equals("l")) {
+            int leftvalue = (int) ((degrees / 360) * 1120);
+            reset_motor();
+            fl.setTargetPosition(-leftvalue);
+            bl.setTargetPosition(-leftvalue);
+            fr.setTargetPosition(leftvalue);
+            br.setTargetPosition(leftvalue);
+            powerBusy(power);
+
+        } else if (l_or_r.equals("r")) {
+            int leftvalue = (int) ((degrees / 360) * 1120);
+            reset_motor();
+            fl.setTargetPosition(leftvalue);
+            bl.setTargetPosition(leftvalue);
+            fr.setTargetPosition(-leftvalue);
+            br.setTargetPosition(-leftvalue);
+            powerBusy(power);
 
         }
+
     }
 }
